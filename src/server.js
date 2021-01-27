ee2f883498db286da0672c9db191d06d1b561470
koderoom <koderoom.dev@gmail.com>
2021-01-27 11:51:34 +0530

    working with post.

--- src/server.js
+++ src/server.js
@@ -13,5 +13,16 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.post("/", (req, res) => {
+  try {
+    const { task, complete } = req.query;
+    todoUpsert.addTodo({ task: task, complete: complete });
+
+    res.json({ message: "Operation Success using POST", opr: true });
+  } catch (err) {
+    res.json({ message: "Server Side Error!", opr: false });
+  }
+});
+
 // T2 Server
 app.listen(3000, () => console.log("Server Started"));
